fd3e8584c474b76cebc810cd2494d991c059624d
Gabriel Fernandez <gabrielfernandezmiranda@gmail.com>
2026-03-05 10:06:36 -0400

    Refactor: Eliminamos botones y hacemos mas estetico el html

--- src/presenter.js
+++ src/presenter.js
@@ -1,39 +1,15 @@
 import calcularPrecioNeto from "./netPrice.js";
 
 const cantidadItemsInput = document.querySelector("#cantidad-items");
-const ventasForm = document.querySelector("#ventas-form");
-const resultadoItemsInput = document.querySelector("#resultado-items");
-
-ventasForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const cantidadItems = cantidadItemsInput.value;
-
-  resultadoItemsInput.value = cantidadItems;
-});
-
-
 const precioItemInput = document.querySelector("#precio-item");
-const precioForm = document.querySelector("#precio-form");
-const resultadoPrecioInput = document.querySelector("#resultado-precio");
-
-precioForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const precioItem = precioItemInput.value;
-
-  resultadoPrecioInput.value = precioItem;
-});
-
-// Precio Neto
 const precioNetoForm = document.querySelector("#precio-neto-form");
 const resultadoPrecioNetoInput = document.querySelector("#resultado-precio-neto");
 
 precioNetoForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const cantidadItems = document.querySelector("#resultado-items").value;
-  const precioItem = document.querySelector("#resultado-precio").value;
+  const cantidadItems = cantidadItemsInput.value;
+  const precioItem = precioItemInput.value;
   const precioNeto = calcularPrecioNeto(cantidadItems, precioItem);
 
   resultadoPrecioNetoInput.value = "Precio neto (" + cantidadItems + "*$" + precioItem + "): $" + precioNeto;
